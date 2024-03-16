a06de8d85833941fab7e45685a4089e99a678760
GOCHI SIYAN <gochi.siyan@gmail.com>
2024-03-17 00:35:04 +0800

    add flask cors to solve cors issue in the frontend

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 import matcher as matcher
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/matcher", methods = ['POST'])
 def match():
